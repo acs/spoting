33bc945906ae458ac262894167c717ef9098492f
Alvaro del Castillo <acs@bitergia.com>
2018-03-25 10:31:28 +0200

    [recommender] Clean artists list and create a mailing list for storing recommendations

--- explorer/recommender.py
+++ explorer/recommender.py
@@ -30,7 +30,8 @@ import os.path
 import explorer
 
 SPOTIFY_USER = 'acsspotify'
-SCOPES = 'user-library-read,user-read-recently-played,user-top-read,user-follow-read'
+SCOPES = 'user-library-read,user-read-recently-played,user-top-read,user-follow-read,playlist-modify-public'
+RECOMMENDER_LIST = "Recommender"
 
 
 def find_artists_recommendations(token, seed_artists_ids=[]):
@@ -84,6 +85,29 @@ def find_related_artists(token, seed_artists_ids=[]):
     return artists
 
 
+def clean_artists(artists, remove=[]):
+    """
+    Remove duplicates and remove list from an artists list
+
+    :param artists: list of artists to be cleaned
+    :param remove: ids of artists to remove
+    :return: the new list of artists
+    """
+
+    cleaned_artists = []
+    already_added_ids = []
+
+    for artist in artists:
+        if artist['id'] in remove:
+            continue
+        elif artist['id'] in already_added_ids:
+            continue
+        else:
+            cleaned_artists.append(artist)
+
+    return cleaned_artists
+
+
 def find_artists(token):
     """
     Get new artists recommendations for the user of the token
@@ -104,6 +128,7 @@ def find_artists(token):
     top_ids = set([a['id'] for a in top])
 
     known_artists = followed_names | top_names
+    known_artists_ids = followed_ids | top_ids
 
     # Getting thew new artists is an expensive operation: cache it
     if os.path.isfile("new-artists.json"):
@@ -126,13 +151,14 @@ def find_artists(token):
         with open("new-artists.json", "w") as fartists:
             json.dump(new_artists, fartists, indent=True)
 
-    # TODO: Remove from new_artists the already followed artists
-
     new_artists_names = set([a['name'] for a in new_artists])
     print("Total new artists to be checked: ", len(new_artists_names - known_artists))
     print("New artists", (new_artists_names - known_artists))
 
-    return new_artists
+    # Clean new_artists: Remove already followed artists and duplicates
+    artists = clean_artists(new_artists, list(known_artists_ids))
+
+    return artists
 
 
 def show_recommendations(new_artists):
@@ -157,10 +183,71 @@ def show_recommendations(new_artists):
     print(json.dumps(artists_followers_sorted, indent=True))
 
 
+def add_artists_top_track(token, playlist, artists):
+    pass
+
+def find_user_playlists(token):
+    """
+    Find all user playlists using scrolling
+
+    :param token: User auth token
+    :return: list of playlists
+    """
+
+    # Check that the list does not exists yet
+    offset = 0
+    max_items = 300
+    limit = 20
+    playlists = []
+
+    while True:
+        playlists_url = explorer.SPOTIFY_API_ME + "/playlists?limit=%i&offset=%i" % (limit, offset)
+        playlists_res = explorer.query_api(token, playlists_url)
+        if not playlists_res['items'] or offset >= max_items:
+            break
+        playlists += playlists_res['items']
+        offset += limit
+
+    return playlists
+
+
+def create_playlist(token, artists):
+    """
+    Creates a new playlist with one song for all the artists
+
+    :param token: User auth token
+    :param artists: list of artists from which to create the playlist
+    :return: None
+    """
+
+    playlists = find_user_playlists(token)
+    recommender_list_id = None
+
+    for playlist in playlists:
+        if playlist['name'] == RECOMMENDER_LIST:
+            recommender_list_id = playlist['id']
+            break
+
+    if not recommender_list_id:
+        # Next create the new mailing list
+        create_url = explorer.SPOTIFY_API + "/users/%s/playlists" % SPOTIFY_USER
+        playlist_data = {
+            "name": "Recommender",
+            "description": "Automatic created playlist with one song for the recommended new artists. https://github.com/acs/spoting."
+        }
+
+        playlist = explorer.query_api(token, create_url, method="POST", data=json.dumps(playlist_data))
+        recommender_list_id = playlist['id']
+
+    # Time to find the tracks and add them to the playlist
+    add_artists_top_track(token, playlist, artists)
+
 
 if __name__ == '__main__':
     token = explorer.collect_tokens(SPOTIFY_USER, SCOPES)
     new_artists = find_artists(token)
     # From all new artists select some of them using filters like genre, followers ...
     show_recommendations(new_artists)
+    # Create a new playlist with one song for each of the new artists
+    create_playlist(token, new_artists)
 
