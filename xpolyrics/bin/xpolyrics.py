d42ab6ad5bd6c5f2cda283e4a0c82106ac88df43
Alvaro del Castillo <acs@bitergia.com>
2018-04-04 00:36:13 +0200

    [xpolyrics] Move script to "bin" directory

--- xpolyrics/bin/xpolyrics.py
+++ xpolyrics/bin/xpolyrics.py
@@ -25,70 +25,17 @@
 
 import requests
 
-from bs4 import BeautifulSoup
-
 from spoting.spoting import collect_tokens, find_user_playlists, find_user_playlist_tracks
+from spoting.genius import build_genius_token, embed_lyrics, find_genius_lyrics, scrap_lyrics
 
 # SPOTIFY config
 SPOTIFY_USER = 'acsspotify'
 SPOTIFY_MARKET = "es"
 SCOPES = 'user-library-read'
 
-# GENIUS config
-GENIUS_API = "https://api.genius.com"
-TOKEN_FILE = ".token-genius"
-
 # LIMITS
 MAX_PLAY_LISTS = 10
 MAX_TRACKS = 50
-
-def build_genius_token():
-    token = None
-
-    with open(TOKEN_FILE) as ftoken:
-        token = ftoken.read()[:-1]
-
-    return token
-
-def scrap_lyrics(lyrics_url):
-    """
-    Scrap the lyrics from Genius HTML lyrics song page. This scrapper must be changed
-    when Genius changes the HTML included in the web lyrics song page.
-
-    :param lyrics_url: Genius song URL
-    :return: a string with the lyrics for the song
-    """
-    page = requests.get(lyrics_url)
-
-    # Lyrics can not be accessed directly from the API
-    # https://genius.com/discussions/277279-Get-the-lyrics-of-a-song
-    # A URL is included with a web page with the lyrics
-    # Scrapping it following: http://www.johnwmillr.com/scraping-genius-lyrics/
-
-    html = BeautifulSoup(page.text, "html.parser")  # Extract the page's HTML as a string
-    lyrics = html.find("div", class_="lyrics").get_text()
-    return lyrics
-
-
-def embed_lyrics(song_id, song_url, song_title, song_author):
-    """
-    Build a Genius viewer to show the lyrics of a song
-
-    :param song_id: Genius song id
-    :param song_url:  Genius song URL
-    :param song_title: Genius song title
-    :param song_author: Genius song author
-    :return: HTML to be included a web page to show the lyrics in Genius viewer
-    """
-
-    embed_viewer = """
-    <div id='rg_embed_link_%s' class='rg_embed_link' data-song-id='%s'>
-        Read <a href='%s' >%s by %s </a> on Genius.
-    </div>
-    <script crossorigin src='//genius.com/songs/1587003/embed.js'></script>
-    """ % (song_id, song_id, song_url, song_title, song_author)
-
-    return embed_viewer
 
 
 if __name__ == '__main__':
@@ -118,11 +65,7 @@ if __name__ == '__main__':
         author = track['track']['artists'][0]['name']
         print("Finding the lyrics for %s by %s" % (song, author))
 
-        url = GENIUS_API + "/search?q=" + song
-        headers = {"Authorization": "Bearer " + token}
-        res = requests.get(url, headers=headers)
-
-        lyrics = res.json()
+        lyrics = find_genius_lyrics(token, song)
 
         if not lyrics['response']['hits']:
             print("Can not find lyrics for %s by %s" % (song, author))
