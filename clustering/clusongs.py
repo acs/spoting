3747100184078ead000e0addde5891944f2b378d
Alvaro del Castillo <acs@bitergia.com>
2018-04-12 07:30:12 +0200

    [clusongs] Feed songs with clustering data to Elasticsearch

--- clustering/clusongs.py
+++ clustering/clusongs.py
@@ -19,15 +19,32 @@
 #
 # Authors:
 #   Alberto Pérez García-Plaza <alpgarcia@bitergia.com>
+#   Alvaro del Castillo <acs@bitergia.com>
 #
+import argparse
 import json
 import string
+
 from pprint import pprint
+
+from elasticsearch import helpers, Elasticsearch
 
 from nltk import word_tokenize, PorterStemmer, collections
 from nltk.corpus import stopwords
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+def get_params():
+    parser = argparse.ArgumentParser(usage="usage: clusongs.py [options]",
+                                     description="Automatic Clustering of songs")
+    parser.add_argument('-d', '--dataset', required=True, help="File path with the songs")
+    parser.add_argument('-e', '--elasticsearch_url', required=False, default='http://localhost:9200',
+                        help="Elasticsearch URL")
+    parser.add_argument('-i', '--elasticsearch_index', required=False, default='songs_clusters',
+                        help="Elasticsearch index in which to store the results")
+
+    return parser.parse_args()
 
 
 def process_text(text, stem=True):
@@ -62,6 +79,34 @@ def cluster_texts(texts, clusters=3):
 
     return clustering
 
+
+def feed_songs(songs, es_url, es_index):
+    """
+    Feed the songs to a Elasticsearch index
+
+    :param songs:  JSON document with the list of songs to feed to Elasticsearch
+    :param es_url:  Elasticsearch URL
+    :param es_index: Elasticsearch index
+    :return: True is all goes well
+    """
+
+    es_conn = Elasticsearch([es_url], timeout=100)
+    docs = []
+
+    # Uploading info to the new ES
+    for item in songs:
+        item_id = item['id']
+        doc = {
+            "_index": es_index,
+            "_type": "item",
+            "_id": item_id,
+            "_source": item
+        }
+        docs.append(doc)
+    helpers.bulk(es_conn, docs)
+    print("Items written to Elasticsearch: " + str(len(docs)))
+
+
 if __name__ == '__main__':
     # Explore some clustering functionalities.
     # Following links have been used:
@@ -72,7 +117,10 @@ if __name__ == '__main__':
     #   >>> nltk.download('punkt')
     #   >>> nltk.download('stopwords')
     #
-    with open('../datasets/bob_dylan_songs.json') as f:
+
+    args = get_params()
+
+    with open(args.dataset) as f:
         songs_json = json.load(f)
 
     lyrics = []
@@ -86,11 +134,16 @@ if __name__ == '__main__':
     clusters = cluster_texts(lyrics, 10)
 
     clusters_with_titles = {}
-    for cluster_id, song_id_list in clusters.items():
+    for cluster_id, song_pos_list in clusters.items():
         cluster_song_list = []
-        for song_id in song_id_list:
-            cluster_song_list.append(titles[song_id])
+        for song_pos in song_pos_list:
+            cluster_song_list.append(titles[song_pos])
+            songs_json[song_pos]['cluster'] = int(cluster_id)
 
         clusters_with_titles[cluster_id] = cluster_song_list
 
     pprint(clusters_with_titles)
+
+    feed_songs(songs_json, args.elasticsearch_url, args.elasticsearch_index)
+
+    print("Songs with cluster information uploaded to", args.elasticsearch_url + "/" + args.elasticsearch_index)
